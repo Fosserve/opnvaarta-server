2ab3d2af3f8d4346a678ec8153fbe8e365af7ae2
hameedullah shaik <hameedsk381@gmail.com>
2025-01-13 19:18:27 +0530

    added vercel

--- api/index.js
+++ api/index.js
@@ -1,9 +1,8 @@
-const express = require("express");
-const connectDB = require("./db/connection");
-const articleRoutes = require("./routes/articles");
-const creatorRoutes = require("./routes/creators"); // Import creator routes
+const express = require("express");// Import creator routes
 const cors = require("cors");
-
+const connectDB = require("../db/connection");
+const articleRoutes = require("../routes/articles");
+const creatorRoutes = require("../routes/creators");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
